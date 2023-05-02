91c61fa0e2e3e321803b15d7a8a5b8ac4eea5fc7
Rayhanali-dev <rayhanalidev@gmail.com>
2023-05-02 17:18:48 +0600

    server setup with chef api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const chef = require('./data/chef.json');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('chef website running')
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.listen(port, () => {
+    console.log(`chef resurant ${port}`);
+})
